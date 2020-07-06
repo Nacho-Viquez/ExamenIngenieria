78dc0a90249eb819319a852c9735c4d33ed8d3ee
Ignacio <joseignacioviquezrojas@gmail.com>
2020-07-06 14:40:44 -0600

    Creacion de la vista para pedir las pizzas

---
+++ PedirPizza/PedirPizza/Models/PedirPizzaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedirPizza.Models
+{
+    public class PedirPizzaController
+    {
+
+        public double CalcularPrecio()
+        {
+            //Datos quemados
+
+
+            double PrecioFinal = 0.0;
+
+            return PrecioFinal;
+        }
+
+
+
+    }
+}
