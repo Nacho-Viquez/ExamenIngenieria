0aa27bba1a283e3d1261579609c2005d11a9c2b5
Ignacio <joseignacioviquezrojas@gmail.com>
2020-07-06 16:22:44 -0600

    Creacion de una clase Pizza para poder almacenar los valores que se solicitan

--- PedirPizza/PedirPizza/Models/PedirPizzaController.cs
+++ PedirPizza/PedirPizza/Models/PedirPizzaController.cs
@@ -10,15 +10,26 @@ namespace PedirPizza.Models
 
         public double CalcularPrecio()
         {
-            //Datos quemados
-
+            //Datos quemados para pruebas 
+            String Tamanio = "mediana";
+            List<String> Coberturas = new List<String>();
+            Coberturas.Add("Hongos");
+            Coberturas.Add("Suprema");
 
             double PrecioFinal = 0.0;
+
 
             return PrecioFinal;
         }
 
+        public double CalcularPrecioCoberturas()
+        {
+            double PrecioCoberturas = 0.0;
 
+
+
+            return PrecioCoberturas;
+        }
 
     }
 }
---
+++ PedirPizza/PedirPizza/Objetos/Pizza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedirPizza.Objetos
+{
+
+   
+    public class Pizza
+    {
+
+        List<String> Coberturas { get; set; }
+
+        String Tamanio { get; set; }
+
+        public Pizza(String Tamanio, List<String> Coberturas)
+        {
+            this.Coberturas = Coberturas;
+
+            this.Tamanio = Tamanio;
+
+        }
+
+    }
+}
